c3cd79edb50f37590272319e370d1c9727d470b3
timohakoper <128609072+timohakoper@users.noreply.github.com>
2023-04-11 22:15:46 +0300

    Add files via upload

---
+++ scroll-top.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) {
+			$('#scroll_top').show();
+		} else {
+			$('#scroll_top').hide();
+		}
+	});
+ 
+	$('#scroll_top').click(function(){
+		$('html, body').animate({scrollTop: 0}, 600);
+		return false;
+	});
+});
