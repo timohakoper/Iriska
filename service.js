64a368236b54a8b44f347a2c767ba17e777748d4
timohakoper <128609072+timohakoper@users.noreply.github.com>
2023-04-28 20:42:18 +0300

    Create service.js

---
+++ service.js
@@ -0,0 +1,12 @@
+let coll = document.getElementsByClassName('collapsible');
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + '20px'
+        }
+    })
+}
